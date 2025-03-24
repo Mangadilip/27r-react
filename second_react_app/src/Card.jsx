7fe4ccf1cdd8bccfb3c63a4b5e2d08fb847449b2
mangadilip <mangadileep42@gmail.com>
2025-03-24 17:36:13 +0530

    card component

---
+++ second_react_app/src/Card.jsx
@@ -0,0 +1,14 @@
+
+// card component..........
+
+function Card(){
+    return(
+    <div className="card">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2FYa32kH0Y7FvuUthcSc-z4Nohya1e6vhow&s" alt="Fortuner img" />
+        <h2>Fortuner</h2>
+        <p>It's a comfortable seven-seater SUV that covers long distances with ease and comfort.</p>
+
+    </div>
+    );
+}
+export default Card;
